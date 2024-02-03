8e4a0c7e361bf813aec807391cdb725c0adaa66e
Michelle Resian <you@example.com>
2024-02-03 22:10:56 +0300

    Hooks event handling

--- src/components/Tickler.js
+++ src/components/Tickler.js
@@ -4,8 +4,6 @@ function Tickler() {
   function tickle() {
     console.log("Teehee!");
   }
-
-  return <button>Tickle me!</button>;
+  return <button onClick={tickle}>Tickle me!</button>;
 }
-
 export default Tickler;
